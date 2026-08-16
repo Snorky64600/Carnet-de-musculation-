b46ed3c3ab985b9c3eb8bebcc88a22fbf6d3ae60
Snorky64600 <cyrius64600@gmail.com>
2026-08-17 00:53:34 +0200

    Create options_screen.dart

---
+++ lib/screens/options_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OPTIONS_OU_AUTRE_Screen extends StatelessWidget { // Change le nom pour chaque fichier (ex: OptionsScreen)
+  const OPTIONS_OU_AUTRE_Screen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Écran en cours de création')),
+      body: const Center(child: Text('En attente de modularisation')),
+    );
+  }
+}
+
