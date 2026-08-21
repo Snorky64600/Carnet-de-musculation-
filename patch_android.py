f5428f28c3cd348f343b80ad6236b60fbeea3a8e
Snorky64600 <cyrius64600@gmail.com>
2026-08-21 17:46:14 +0200

    Update patch_android.py

--- patch_android.py
+++ patch_android.py
@@ -12,30 +12,12 @@ for path in glob.glob("clean_build/android/**/*.gradle", recursive=True) + ["cle
 path_app = "clean_build/android/app/build.gradle"
 with open(path_app, "r") as f: content = f.read()
 content = content.replace("minSdkVersion 19", "minSdkVersion 26").replace("minSdkVersion flutter.minSdkVersion", "minSdkVersion 26")
-
-# Injection signature et config
-signing_block = """
-android {
-    signingConfigs {
-        release {
-            keyAlias 'key'
-            keyPassword 'password'
-            storeFile file('key.jks')
-            storePassword 'password'
-        }
-    }
-    buildTypes {
-        release { signingConfig signingConfigs.release }
-    }
-"""
-content = content.replace("android {", signing_block)
 with open(path_app, "w") as f: f.write(content)
 
-# 3. Mise à jour critique : AndroidManifest.xml (Permissions + Service Health Connect)
+# 3. AndroidManifest.xml (Permissions + Service Health Connect)
 manifest_path = "clean_build/android/app/src/main/AndroidManifest.xml"
 with open(manifest_path, "r") as f: manifest_content = f.read()
 
-# On insère les permissions et le service avant <application>
 inject = """
     <uses-permission android:name="android.permission.health.READ_HEART_RATE"/>
     <uses-permission android:name="android.permission.health.READ_EXERCISE"/>
@@ -43,10 +25,8 @@ inject = """
         <package android:name="com.google.android.apps.healthdata" />
     </queries>
     <application"""
-
 manifest_content = manifest_content.replace("<application", inject)
 
-# On ajoute le service Health Connect à l'intérieur de <application>
 service_entry = """
         <service
             android:name="androidx.health.connect.client.impl.converters.datatype.HealthConnectClientProvider"
@@ -58,7 +38,6 @@ service_entry = """
         </service>
 """
 manifest_content = manifest_content.replace("</activity>", "</activity>" + service_entry)
-
 with open(manifest_path, "w") as f: f.write(manifest_content)
 
 # 4. MainActivity
