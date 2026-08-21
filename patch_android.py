a7f8881db2ce6cf35d096627200f57d5dc1bdfc0
Snorky64600 <cyrius64600@gmail.com>
2026-08-21 17:59:03 +0200

    Update patch_android.py

--- patch_android.py
+++ patch_android.py
@@ -8,39 +8,44 @@ for path in glob.glob("clean_build/android/**/*.gradle", recursive=True) + ["cle
         with open(path, "w") as f: f.write(content)
     except: pass
 
-# 2. Mise à jour de app/build.gradle
+# 2. Configuration du minSdk à 26 (requis par Health Connect)
 path_app = "clean_build/android/app/build.gradle"
 with open(path_app, "r") as f: content = f.read()
 content = content.replace("minSdkVersion 19", "minSdkVersion 26").replace("minSdkVersion flutter.minSdkVersion", "minSdkVersion 26")
 with open(path_app, "w") as f: f.write(content)
 
-# 3. AndroidManifest.xml (Permissions + Service Health Connect)
+# 3. Injection des permissions et déclaration Santé Connect
 manifest_path = "clean_build/android/app/src/main/AndroidManifest.xml"
 with open(manifest_path, "r") as f: manifest_content = f.read()
 
-inject = """
+permissions_and_queries = """
     <uses-permission android:name="android.permission.health.READ_HEART_RATE"/>
     <uses-permission android:name="android.permission.health.READ_EXERCISE"/>
     <queries>
         <package android:name="com.google.android.apps.healthdata" />
     </queries>
     <application"""
-manifest_content = manifest_content.replace("<application", inject)
 
-service_entry = """
-        <service
-            android:name="androidx.health.connect.client.impl.converters.datatype.HealthConnectClientProvider"
+manifest_content = manifest_content.replace("<application", permissions_and_queries)
+
+# Déclaration obligatoire pour l'affichage dans Santé Connect
+health_alias = """
+        <activity-alias
+            android:name="ViewPermissionUsageActivity"
             android:exported="true"
-            android:permission="android.permission.BIND_HEALTH_CONNECT_SERVICE">
+            android:targetActivity=".MainActivity"
+            android:permission="android.permission.START_VIEW_PERMISSION_USAGE">
             <intent-filter>
-                <action android:name="androidx.health.connect.client.action.HEALTH_CONNECT_SERVICE" />
+                <action android:name="android.intent.action.VIEW_PERMISSION_USAGE" />
+                <category android:name="android.intent.category.HEALTH_PERMISSIONS" />
             </intent-filter>
-        </service>
+        </activity-alias>
 """
-manifest_content = manifest_content.replace("</activity>", "</activity>" + service_entry)
+manifest_content = manifest_content.replace("</activity>", "</activity>\n" + health_alias)
+
 with open(manifest_path, "w") as f: f.write(manifest_content)
 
-# 4. MainActivity
+# 4. Conversion MainActivity en FlutterFragmentActivity
 for path in glob.glob("clean_build/android/app/src/main/kotlin/**/MainActivity.kt", recursive=True):
     with open(path, "w") as f:
         f.write("""package com.cyril.muscu.carnet_musculation;
